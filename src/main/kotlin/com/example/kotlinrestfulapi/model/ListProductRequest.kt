d2677ebbbdf394418d5e51333debc4e3b9549ced
Ryan Krnwn <ryankrnwn019@gmail.com>
2022-08-22 14:58:13 +0700

    first commit

---
+++ src/main/kotlin/com/example/kotlinrestfulapi/model/ListProductRequest.kt
@@ -0,0 +1,9 @@
+package com.example.kotlinrestfulapi.model
+
+data class ListProductRequest(
+
+    val page: Int,
+
+    val size: Int
+
+)
---
+++ src/main/kotlin/com/example/kotlinrestfulapi/model/WebResponse.kt
@@ -0,0 +1,9 @@
+package com.example.kotlinrestfulapi.model
+
+data class WebResponse<T>(
+    val code: Int,
+
+    val status: String,
+
+    val data: T
+)
