81aba9098bddf8105a409d264eca0a45ac24563b
Ryan Krnwn <ryankrnwn019@gmail.com>
2022-08-23 08:04:42 +0700

    tested deploy on server vps

---
+++ learn-docker/Login_SSH_VPS_API.bat
@@ -0,0 +1,9 @@
+@echo off
+cls
+echo #######################################
+echo Main IP: your ip
+echo Root pass: your password
+echo #######################################
+ssh root@api.example.com -p 22
+pause
+Login_SSH_VPS_API.bat
